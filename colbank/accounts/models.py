59e4c73b3f4a18090fb2cf174359cdc3458d2477
gideonclottey <gideonclotteyint@gmail.com>
2023-02-26 02:19:37 +0000

    Setting up django Boilerplate

---
+++ colbank/accounts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Account(models.Model):
+    pass
+class Customer(models.Model):
+    pass
+
+class Transfer(models.Model):
+    pass
+
+class Widthrawal(models.Model):
+    pass
+
+class Deposit(models.Model):
+    pass
