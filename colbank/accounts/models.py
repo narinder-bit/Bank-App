cece12f2ed07e54e294ffe96a74d35d1a803ea47
Narinder <narinderkaur150@gmail.com>
2023-03-06 23:42:46 +0000

    deposit transaction

--- colbank/accounts/models.py
+++ colbank/accounts/models.py
@@ -14,4 +14,7 @@ class Widthrawal(models.Model):
     pass
 
 class Deposit(models.Model):
-    pass
+    account_id = model.models.ForeignKey("Account", verbose_name=_(""), on_delete=models.CASCADE)
+    amount = model.models.DecimalField( max_digits=10, decimal_places=2)
+    date_created = models.models.DateField( auto_now_add=True)
+    
