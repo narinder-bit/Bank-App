047fd1b451d40b9f014e3231483b8db3cef956fd
Asun <ozomata@live.com>
2023-03-06 23:07:09 +0000

    Create Costumer Class

--- colbank/accounts/models.py
+++ colbank/accounts/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Account(models.Model):
     pass
 class Customer(models.Model):
-    pass
+     # Fields
+     user_name = models.CharField(max_length=20, help_text='Enter  username')
+     user_email = models.CharField(max_length=20, help_text='Enter email address')
+     user= models.ForeignKey(User, on_delete=models.CASCADE)
+     
 
 class Transfer(models.Model):
     pass
