d2078918af1fb2a4ef8db510b5415b0c96a853b1
daudugodswill97 <daudugodswill2019@gmail.com>
2023-03-06 23:22:08 +0000

    Modified Account Model in models.py

--- colbank/accounts/models.py
+++ colbank/accounts/models.py
@@ -2,8 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+# This model includes three fields: 'account_number', 'balance', and 'customer'.
+# 'account_number' is a unique string that identifies the account
+# 'balance' is a decimal number representing the account balance, and 'customer'
+# is a foreign key relationship to the 'customer' model, indicating which customer owns the account.
+
 class Account(models.Model):
-    pass
+    account_number = models.CharField(max_length=20, unique=True)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.account_number
+
 class Customer(models.Model):
     pass
 
