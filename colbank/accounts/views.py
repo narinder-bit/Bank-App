6a30892a05f774729f8064a38c318ea45e13ee97
gideonclottey <gideonclotteyint@gmail.com>
2023-03-07 23:18:12 +0000

    Creating the home view and folder restructuring

--- colbank/accounts/views.py
+++ colbank/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from . models import Deposit
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home_list.html', {})
+
+class DepositCreateView(CreateView):
+    model = Deposit
--- colbank/colbank/settings.py
+++ colbank/colbank/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'colbank.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'static/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS =[
+    BASE_DIR /'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
